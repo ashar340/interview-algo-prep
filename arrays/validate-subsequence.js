34ecea9871ec825eafe3ce42edc23ca75bdeff30
Ananya <ananyas237@gmail.com>
2020-10-05 22:38:06 -0400

    isValidSubsequence added

---
+++ arrays/validate-subsequence.js
@@ -0,0 +1,21 @@
+function isValidSubsequence(array, sequence) {
+    // Write your code here.
+      let leftIdx, targetIdx = 0;
+      let rightIdx = array.length - 1;
+  
+      while(leftIdx < rightIdx) {
+          if(sequence[targetIdx] == array[leftIdx]) {
+              targetIdx++;
+              leftIdx++; 
+          }
+          else {
+              leftIdx++;
+          }
+      }
+      return targetIdx == sequence.length-1;
+  }
+  
+  // Do not edit the line below.
+  exports.isValidSubsequence = isValidSubsequence;
+
+  isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1,6,-1,10])
